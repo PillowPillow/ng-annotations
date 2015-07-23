cd4cfa939933eca73539969361cdefc9a4277be2
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2015-07-24 01:04:08 +0200

    added autobind decorator

---
+++ src/decorators/utils/autobind.js
@@ -0,0 +1,27 @@
+/**
+ * @decorator: @autobind
+ * @type: statement
+ *
+ * bind a method to its current context
+ *
+ */
+export default function autobind(props, name, descriptor) {
+
+	let fn = descriptor.value;
+
+	if(typeof fn !== 'function')
+		throw Error(`@autobind decorator can only be applied to methods not: ${typeof fn}`);
+
+	return {
+		configurable: true,
+		get: function get() {
+			var boundFn = fn.bind(this);
+			Object.defineProperty(this, name, {
+				value: boundFn,
+				configurable: true,
+				writable: true
+			});
+			return boundFn;
+		}
+	};
+}

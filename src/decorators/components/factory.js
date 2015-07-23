c2d133d41af930cec56a86fdc82bd0814a11d660
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2015-07-24 01:07:53 +0200

    added factory decorator

---
+++ src/decorators/components/factory.js
@@ -0,0 +1,33 @@
+import utils from 'src/libs/utils';
+import inject from 'src/decorators/utils/inject';
+
+/**
+ * @decorator: @factory
+ * @type: function
+ *
+ * declares a new angular factory
+ *
+ * @param name (optional)  replaces the class name
+ *
+ * @returns {Function}
+ */
+export default function NgFactory(name = '') {
+	return (target) => {
+		name = name || target.name;
+
+		var component = function(...injections) {
+			let factory = new target(...injections);
+            return factory.expose instanceof Function ? factory.expose() : factory;
+		}
+
+		if(!(target.$inject instanceof Array) || target.$inject.length === 0) {
+			var parameters = utils.extractParameters(target);
+			if(parameters.length > 0)
+				inject(parameters)(component);
+		}
+		else inject(target.$inject)(component);
+
+		utils.addDeclareMethod(target);
+		utils.defineComponent(target, name, 'factory', component);
+	}
+}

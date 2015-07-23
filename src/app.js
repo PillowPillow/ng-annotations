d28a9aeb896be0be55de39604eb70f1f12032863
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2015-07-24 01:07:23 +0200

    added config decorator

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 export var controller = require('src/decorators/components/controller');
 export var service = require('src/decorators/components/service');
 export var animation = require('src/decorators/components/animation');
+export var config = require('src/decorators/components/config');
 
 // utils
 export var inject = require('src/decorators/utils/inject');
---
+++ src/decorators/components/config.js
@@ -0,0 +1,32 @@
+import utils from 'src/libs/utils';
+import inject from 'src/decorators/utils/inject';
+
+/**
+ * @decorator: @config
+ * @type: function
+ *
+ * declares a new angular config
+ *
+ * @param name (optional)  replaces the class name
+ *
+ * @returns {Function}
+ */
+export default function NgConfig() {
+	return (target) => {
+
+		var component = function(...injections) {
+            let config = new target(...injections);
+		}
+
+		if(!(target.$inject instanceof Array) || target.$inject.length === 0) {
+			var parameters = utils.extractParameters(target);
+			if(parameters.length > 0)
+				inject(parameters)(component);
+		}
+		else inject(target.$inject)(component);
+
+
+		utils.addDeclareMethod(target);
+		utils.defineComponent(target, null, 'config', component);
+	}
+}

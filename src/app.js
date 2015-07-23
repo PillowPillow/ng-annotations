4c35c5baca7dc6c14c39f7fdf486fbcedeaf9c84
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2015-07-24 01:07:12 +0200

    added animation decorator

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // components
 export var controller = require('src/decorators/components/controller');
 export var service = require('src/decorators/components/service');
+export var animation = require('src/decorators/components/animation');
 
 // utils
 export var inject = require('src/decorators/utils/inject');
---
+++ src/decorators/components/animation.js
@@ -0,0 +1,32 @@
+import utils from 'src/libs/utils';
+import inject from 'src/decorators/utils/inject';
+
+/**
+ * @decorator: @animation
+ * @type: function
+ *
+ * declares a new angular animation
+ *
+ * @param name (optional)  replaces the class name
+ *
+ * @returns {Function}
+ */
+export default function NgAnimation(name = '') {
+	return (target) => {
+		name = name || target.name;
+
+		var component = function(...injections) {
+            return new target(...injections);
+		}
+
+		if(!(target.$inject instanceof Array) || target.$inject.length === 0) {
+			var parameters = utils.extractParameters(target);
+			if(parameters.length > 0)
+				inject(parameters)(component);
+		}
+		else inject(target.$inject)(component);
+
+		utils.addDeclareMethod(target);
+		utils.defineComponent(target, name, 'animation', component);
+	}
+}

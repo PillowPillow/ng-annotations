b964069a1eefd7d46a2628d9303a043a268943c0
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2015-07-24 01:04:20 +0200

    typo

--- src/app.js
+++ src/app.js
@@ -3,4 +3,4 @@ export var controller = require('src/decorators/components/controller');
 
 // utils
 export var inject = require('src/decorators/utils/inject');
-export var inject = require('src/decorators/utils/autobind');
+export var autobind = require('src/decorators/utils/autobind');

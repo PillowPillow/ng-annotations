53023211e2f32a53d9419f5502f54ce2947e5b36
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2015-07-24 00:50:58 +0200

    added webpack configs

---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,80 @@
+var config = {};
+
+/*modules*/
+var Path = require('path'),
+	webpack = require('webpack');
+
+/*consts*/
+var project = Path.join(__dirname, '..'),
+	wdir = Path.join(project, 'example/assets'),
+	entryfile = Path.join(wdir, 'app.js'),
+	src = Path.join(project, 'src'),
+	styles = Path.join(wdir, 'styles'),
+	node_modules = Path.join(project, 'node_modules'),
+	bower_components = Path.join(project, 'bower_components');
+
+/*app folder paths*/
+var modules = Path.join(wdir, 'modules');
+
+config.entryfile = entryfile;
+config.devtool = 'eval';
+config.entry = {};
+config.entry.vendors = ['angular'];
+config.entry.app = [
+    'webpack/hot/dev-server',
+    entryfile
+];
+
+config.cache = true;
+
+config.output = {
+	publicPath: '/',
+	filename: 'app.js',
+	chunkFilename: '[chunkhash].bundle.js'
+};
+
+config.resolve = {};
+config.resolve.extensions = ['', '.js', '.jsx', '.scss'];
+config.resolve.root = modules;
+
+config.module = {};
+config.module.loaders = [
+	{ test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+	{ test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff2" },
+	{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+	{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+	{ test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+    { test: /\.jade$/, loader: 'jade' },
+	{ test: /\.css/, loader : 'style!css' },
+	{ test: /\.scss$/, loader : 'style!css!sass' },
+    {
+        test: /\.js$/,
+        loader: 'babel?experimental&optional=es7',
+        exclude: [node_modules, bower_components]
+    },
+    {
+        test: /\.jsx$/,
+        loader: 'babel?experimental&optional=es7',
+        exclude: [node_modules, bower_components]
+    }
+];
+
+
+config.resolve.alias = {
+	styles: styles,
+	modules: modules,
+	bower: bower_components,
+	npm: node_modules,
+	src: src,
+	core: entryfile
+};
+
+config.plugins = [
+    new webpack.optimize.OccurenceOrderPlugin(),
+	new webpack.optimize.DedupePlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+];
+
+module.exports = config;
---
+++ webpack/webpack.dist.config.js
@@ -0,0 +1,69 @@
+var config = {};
+
+/*modules*/
+var Path = require('path'),
+	webpack = require('webpack'),
+	pckg = require('../package.json')
+
+/*consts*/
+var project = Path.join(__dirname, '..'),
+	wdir = Path.join(project, 'src'),
+	entryfile = Path.join(wdir, 'app.js'),
+	dist = Path.join(project, 'dist'),
+	styles = Path.join(wdir, 'styles'),
+	node_modules = Path.join(project, 'node_modules'),
+	bower_components = Path.join(project, 'bower_components');
+
+config.entryfile = entryfile;
+config.devtool = 'source-map';
+console.log(entryfile)
+config.entry = entryfile;
+
+config.cache = true;
+
+config.output = {
+	publicPath: '/',
+	filename: pckg.name + '.js',
+	path: dist,
+	chunkFilename: '[chunkhash].bundle.js'
+};
+
+config.resolve = {};
+config.resolve.extensions = ['', '.js', '.jsx'];
+config.resolve.root = wdir;
+
+config.module = {};
+config.module.loaders = [
+    {
+        test: /\.js$/,
+        loader: 'babel?experimental&optional=es7',
+        exclude: [node_modules, bower_components]
+    },
+    {
+        test: /\.jsx$/,
+        loader: 'babel?experimental&optional=es7',
+        exclude: [node_modules, bower_components]
+    }
+];
+
+
+config.resolve.alias = {
+	src: wdir,
+	core: entryfile
+};
+
+config.plugins = [
+    new webpack.optimize.OccurenceOrderPlugin(),
+	new webpack.optimize.DedupePlugin(),
+    new webpack.NoErrorsPlugin(),
+	new webpack.optimize.UglifyJsPlugin({
+		minimize: true,
+		sourceMap: true,
+		compress: { warnings: false },
+		mangle: {
+			except: ['exports', 'require', 'module']
+		}
+	})
+];
+
+module.exports = config;

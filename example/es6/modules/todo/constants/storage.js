bfd46e1782b8eddc7bb6878831f10d93180d9f74
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2015-07-24 18:00:11 +0200

    updated author

--- example/es6/modules/todo/constants/storage.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-(function () {
-	var _ngAnnotations = ngAnnotations;
-	var constant = _ngAnnotations.constant;
-
-	constant('storage-id', 'angular-todo-mvc-id').autodeclare('todomvc');
-})();

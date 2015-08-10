a8b8b52da7ebfec6f7f6b59266548f0ded19d960
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2015-08-10 18:53:41 +0200

    added attach decorator

--- src/decorators/utils/attach.js
+++ src/decorators/utils/attach.js
@@ -28,35 +28,73 @@ export default function attach(source = 'this', path = '') {
 		if(source instanceof Object)
 			source = source.$name;
 
-		if(prototype.$attach === undefined
-		|| !(prototype.$attach instanceof Array))
-			prototype.$attach = [];
+		if(prototype.$transform === undefined
+		|| !(prototype.$transform instanceof Array))
+			prototype.$transform = [];
 
-		let pathSteps = path.split('.'),
-			propertyName = pathSteps.pop();
+		let steps = path.split('.'),
+			propertyName = steps.pop();
 
 		if(source === 'this') {
-			delete descriptor.initializer;/*babel patch*/
+			delete descriptor.initializer;
 			delete descriptor.value;
-			descriptor.get = function() {
-				let src = getSrc(this, pathSteps);
-				return src[propertyName];
-			};
-			descriptor.set = function(val) {
-				let src = getSrc(this, pathSteps);
-				src[propertyName] = val;
-			};
+			setDescriptor(source,steps,propertyName,descriptor);
 		}
-		else prototype.$attach.push({
-			source,
-			path,
-			pathSteps,
-			propertyName,
-			target: name
-		});
+		else
+			prototype.$transform.push(getApplyTransformation(source,steps,propertyName,name));
 	}
 }
 
+/**
+ * @param sourceName    String. name of the source component
+ * @param steps Array. path toward the property
+ * @param propertyName  String. property name
+ * @param targetName    String. name of the target property
+ * @returns {Function}
+ */
+function getApplyTransformation(sourceName, steps, propertyName, targetName) {
+	return function attachTransformation(context, component, injections) {
+
+		let $inject = component.$inject || [],
+			index = $inject.indexOf(sourceName);
+		if(!~index)
+			throw Error(`unable to attach the property ${propertyName}, the component ${sourceName} isn't loaded`)
+
+		let {configurable, enumerable} = Object.getOwnPropertyDescriptor(context, targetName);
+		let descriptor = {configurable, enumerable};
+		setDescriptor(sourceName, steps, propertyName, descriptor, injections[index]);
+		delete context[targetName];
+		Object.defineProperty(context, targetName, descriptor);
+	}
+}
+
+/**
+ * @param source    Object. source object
+ * @param steps Array. path toward the property
+ * @param property  String. property name
+ * @param descriptor    Object. property descriptor
+ * @param context   (optional) Object. exec context
+ */
+function setDescriptor(source, steps, property, descriptor, context = undefined) {
+	descriptor.get = function() {
+		if(context === undefined)
+			context = this;
+		let src = getSrc(context, steps);
+		return src[property] instanceof Function ? src[property].bind(src) : src[property];
+	};
+	descriptor.set = function(val) {
+		if(context === undefined)
+			context = this;
+		let src = getSrc(context, steps);
+		src[property] = val;
+	};
+}
+
+
+/**
+ * @param source    Object. source object
+ * @param path Array. path toward the property
+ */
 function getSrc(source, path = []) {
 	if(path.length > 0)
 		for(var i=0; i<path.length; i++) {

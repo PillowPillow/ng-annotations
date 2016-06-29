dd831f33226586c7b445254b6a7a6473e66435d1
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2016-06-29 18:50:21 +0200

    update inject method - now support target variance

--- src/decorators/utils/inject.js
+++ src/decorators/utils/inject.js
@@ -7,24 +7,27 @@
  * @param toInject  string|Array
  * @param more (optional)  string[]
  */
-export default function inject(toInject, ...more) {
-
+export function inject(toInject, ...more) {
 	if(!(toInject instanceof Array)) {
 		toInject = [toInject];
 		if(more.length > 0)
 			toInject = toInject.concat(more);
 	}
-
 	toInject.forEach((component, index) => {
 		if(component instanceof Object && '$name' in component)
 			toInject[index] = component.$name;
 	});
 
+	return injectTo(toInject, '$inject');
+}
+
+export function injectTo(toInject, targetField) {
 	return (target, ...options) => {
 
 		if(options.length > 0)
 			target = options[1].value;
-		Object.defineProperty(target, '$inject', {
+
+		Object.defineProperty(target, targetField, {
 			value: toInject,
 			enumerable: true,
 			configurable: true

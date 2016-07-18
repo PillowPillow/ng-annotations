566d1301a75d927f8d6c8a4ab80a8c0ccf511348
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2016-07-19 01:51:32 +0200

    add angular mock to the spec bundle

--- config/spec-bundle.js
+++ config/spec-bundle.js
@@ -1,9 +1,11 @@
 Error.stackTraceLimit = Infinity;
 
 var context = require.context('../lib', true, /\.spec\.js/);
+global.angular = require('angular');
+require('angular-mocks');
 
 function requireAll(requireContext) {
-  return requireContext.keys().map(requireContext);
+	return requireContext.keys().map(requireContext);
 }
 
 var modules = requireAll(context);

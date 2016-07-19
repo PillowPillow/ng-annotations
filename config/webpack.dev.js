8c914f49da14f9430297aae0d2b0a436d4b89258
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2016-07-19 05:41:41 +0200

    update webpack dev config

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -9,5 +9,21 @@ module.exports = wmerge(common, {
 	entry: [
 		'webpack/hot/dev-server',
 		'./example/app'
-	]
+	],
+	module: {
+		loaders: [
+			{test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
+			{test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff2"},
+			{test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
+			{test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
+			{test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"},
+			{test: /\.jade$/, loader: 'jade'},
+			{test: /\.css/, loader: 'style!css'},
+			]
+	},
+	resolve: {
+		alias: {
+			lib: __dirname + '/../lib'
+		}
+	}
 });
